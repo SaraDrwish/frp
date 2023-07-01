29128c75373135d0ed9e3238cac2efb649bac010
Sara <38770404+SaraDrwish@users.noreply.github.com>
2023-07-01 10:22:40 +0300

    first commit

---
+++ src/component/PasicProject.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import { Link , Outlet} from "react-router-dom";
+
+
+function PasicProject() {
+  return (
+    <div className='PasicProject' id='PasicProject'>
+      <h4> love HTML & CSS ProJects </h4>
+    </div>
+    
+  )
+}
+
+export default PasicProject
